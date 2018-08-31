dd8dbd4ea223644787d7a8c2af3d0764a5dc0d94
aashritagoel <aashrita.goel42@gmail.com>
2018-08-31 10:01:38 +0530

    Added play project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
-rm -r seed-1.0-SNAPSHOT | echo "artifact already deleted"
+ps -ef | grep seed-1.0-SNAPSHOT | grep -v grep | awk '{print $2}' | xargs kill
+rm -rf seed-1.0-SNAPSHOT
 unzip seed-1.0-SNAPSHOT.zip
-nohup ./seed-1.0-SNAPSHOT/bin/seed &
+./seed-1.0-SNAPSHOT/bin/seed &
+rm -rf seed-1.0-SNAPSHOT.zip
+rm -rf play.out
+rm -rf logs
